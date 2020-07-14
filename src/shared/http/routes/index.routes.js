319ac44149376c9c6c6f9e3dabf4391af17f9871
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-14 19:39:10 -0300

    feat: home page

--- src/shared/http/routes/index.routes.js
+++ src/shared/http/routes/index.routes.js
@@ -2,10 +2,17 @@ import { Router } from "express";
 import categoriesRouter from "../../../modules/categories/routes/categories.routes";
 import articlesRouter from "../../../modules/articles/routes/articles.routes";
 import adminRouter from "../../../modules/admin/routes/admin.routes";
+import Article from "../../../modules/articles/app/models/Article";
 const routes = Router();
 
 routes.use("/categories", categoriesRouter);
 routes.use("/admin", adminRouter);
 routes.use("/articles", articlesRouter);
+
+routes.get("/", (req, res) => {
+  Article.findAll().then((articles) => {
+    res.render("index.ejs", { articles });
+  });
+});
 
 export default routes;
