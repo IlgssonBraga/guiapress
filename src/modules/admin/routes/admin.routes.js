80933ff989e0e695cdef1721ac78b553a3dbd93f
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-14 11:52:38 -0300

    feat: list categories

--- src/modules/admin/routes/admin.routes.js
+++ src/modules/admin/routes/admin.routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import Category from "../../categories/app/models/Category";
 
 const adminRouter = Router();
 
@@ -8,5 +9,11 @@ adminRouter.get("/", (req, res) => {
 
 adminRouter.get("/categories/new", (req, res) => {
   res.render("admin/categories/new.ejs");
+});
+
+adminRouter.get("/categories", (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render("admin/categories/index.ejs", { categories });
+  });
 });
 export default adminRouter;
