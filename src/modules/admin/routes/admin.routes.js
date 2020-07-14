47bc041e6a68147e9bdd06e468971b4554a93467
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-14 12:15:02 -0300

    feat: delete categories

--- src/modules/admin/routes/admin.routes.js
+++ src/modules/admin/routes/admin.routes.js
@@ -11,6 +11,22 @@ adminRouter.get("/categories/new", (req, res) => {
   res.render("admin/categories/new.ejs");
 });
 
+adminRouter.post("/categories/delete", (req, res) => {
+  const id = req.body.id;
+
+  if (id) {
+    if (!isNaN(id)) {
+      Category.destroy({ where: { id } }).then(() =>
+        res.redirect("/admin/categories")
+      );
+    } else {
+      res.redirect("/admin/categories");
+    }
+  } else {
+    res.redirect("/admin/categories");
+  }
+});
+
 adminRouter.get("/categories", (req, res) => {
   Category.findAll().then((categories) => {
     res.render("admin/categories/index.ejs", { categories });
