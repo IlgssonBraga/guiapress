b664e5111dfd5dc56aef7b5326a20b0b5509f02a
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-14 11:08:31 -0300

    feat: relationship between models

--- src/modules/articles/app/models/Article.js
+++ src/modules/articles/app/models/Article.js
@@ -1,5 +1,6 @@
 import Sequelize from "sequelize";
 import connection from "../../../../shared/config/database";
+import Category from "../../../categories/app/models/Category";
 
 const Article = connection.define("articles", {
   title: {
@@ -15,5 +16,10 @@ const Article = connection.define("articles", {
     allowNull: false,
   },
 });
+
+Category.hasMany(Article);
+Article.belongsTo(Category);
+
+// Article.sync({ force: true });
 
 export default Article;
