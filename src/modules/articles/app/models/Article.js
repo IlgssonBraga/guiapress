8c7cf394dff1c1de9bd20a82e047429b1d94a5a6
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-14 10:56:57 -0300

    feat: article model

---
+++ src/modules/articles/app/models/Article.js
@@ -0,0 +1,19 @@
+import Sequelize from "sequelize";
+import connection from "../../../../shared/config/database";
+
+const Article = connection.define("articles", {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  body: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+});
+
+export default Article;
